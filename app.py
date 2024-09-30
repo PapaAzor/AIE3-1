47b29f0239497175deaddabf60c684850bf9992e
AdamGr <134016956+PapaAzor@users.noreply.github.com>
2024-09-30 10:39:42 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template
+import socket
+
+app = Flask(__name__)
+
+ROBOT_IP = '192.168.0.100'  #example
+ROBOT_PORT = 8000  #port for the TCP server
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#sends commands 
+def send_command(command):
+    #use socket.socket(addr,type) then connect and send 
+#    try:
+#        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#            s.connect((ROBOT_IP, ROBOT_PORT))
+#            s.sendall(command.encode('utf-8'))
+
+    print(f"Simulating sending command: {command}")
+
+startSet=False
+endSet=False
+endPoint=[0,0]
+startPoint=[0,0]
+
+@app.route('/move/<endpoint>')
+def move(endpoint):
+    endPoint[0]=endpoint[0]
+    endPoint[1]=endpoint[2]#endpoint is sent as a string I guess so [1] would return a ','
+    global endSet
+    endSet=True
+    
+    send_command(endpoint)
+    print("Received data:", endpoint) 
+    findingWay(startPoint,endPoint)
+    return {"status": "success", "message": f"Command received: {endpoint}"}, 200 
+    
+@app.route('/setStartingPosition/<coordinates>')
+def setStartingPosition(coordinates):
+    
+    startPoint[0]=coordinates[0]
+    startPoint[1]=coordinates[2]#same as for endpoint
+    global startSet
+    startSet= True
+    x, y = coordinates.split(',')
+    findingWay(startPoint,endPoint)
+    print(f"Starting position received: ", coordinates)
+    return f"Starting position set to ({x}, {y})"
+
+
+def findingWay(start,end):
+    if(startSet==True and endSet==True):
+        print("Need to find way from: ",startPoint," to: ",endPoint)
+    else :
+        print("endpoint:", endPoint)
+        print(endSet)
+        print("starPoint: ",startPoint)
+        print(startSet)
+    
+
+#reset function
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
