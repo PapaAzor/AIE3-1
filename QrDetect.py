9559684a55ca479e71b506a648bc161daa983c06
AdamGr <134016956+PapaAzor@users.noreply.github.com>
2024-10-30 09:18:43 +0100

    Update and rename QrTest.py to QrDetect.py

---
+++ QrDetect.py
@@ -0,0 +1,65 @@
+import sqlite3
+import cv2
+from flask import Flask, render_template,jsonify, request, g #json for convertion, request obvious and g for storing data(typically databvase)
+import socket
+
+width = 1280
+height = 720
+camera_id = 0
+delay = 1
+window_name = 'OpenCV QR Code'
+
+qcd = cv2.QRCodeDetector()
+cam = cv2.VideoCapture(camera_id)
+
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+cam.set(cv2.CAP_PROP_FPS,15)
+cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
+
+def get_db():
+    
+    db=sqlite3.connect("sia.db")
+    return db
+        
+    
+def scan_qr(qr_code):
+    db = get_db()
+    cursor = db.cursor() # something that allows interaction with the database
+    
+    cursor.execute('SELECT shape, x_position, y_position FROM qr_positions WHERE qr_data = ?', (qr_code,))
+    result = cursor.fetchone()
+
+    if result:
+        shape, x_pos, y_pos = result  
+        return shape, x_pos, y_pos
+    return None  
+        
+def qr_func(ret,frame):
+    if ret:
+        ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
+        if ret_qr:
+            for s, p in zip(decoded_info, points):
+                if s:
+                    
+                    result=scan_qr(s)
+                    color = (0, 255, 0)
+                    if result:
+                        shape, x_pos, y_pos = result
+                        print(f"Shape: {shape}, X: {x_pos}, Y: {y_pos}")
+                    
+                else:
+                    color = (0, 0, 255)
+                frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)    
+
+get_db()
+while True:
+    
+    ret, frame = cam.read()
+    qr_func(ret,frame)
+    cv2.imshow(window_name, frame)
+
+    if cv2.waitKey(delay) & 0xFF == ord('q'):
+        break
+
+cv2.destroyWindow(window_name)
