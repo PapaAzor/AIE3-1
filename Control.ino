9b88d948e93870b322d86f2617c1390fbe7ab28a
AdamGr <134016956+PapaAzor@users.noreply.github.com>
2024-11-11 12:50:54 +0100

    Update Control.ino

--- Control.ino
+++ Control.ino
@@ -8,7 +8,7 @@
 MPU6050 mpu(Wire);
 unsigned long timer = 0;
 int angle;
-
+ int desiredAngle=0;// na teraz, przy skrecie 90 ,180 itd
 double straight(){
 
   mpu.update();
@@ -19,31 +19,24 @@ double straight(){
 
   digitalWrite(dir_1,HIGH); //RIGHT WHEEL FORWARD WHEN HIGH
   digitalWrite(dir_2,LOW); //LEFT WHEEL BACKWARD WHEN HIGH
-  //float multiplier=abs(angle/100)+1;
-  int correction=0;
-  if(angle==0){
+ 
+  int error=desiredAngle-angle;
+  if(error==0){
     Serial.println("condition 1");
   analogWrite(pwm_2, 140); 
   analogWrite(pwm_1, 140);
   }
-  if(angle>0){
-    Serial.println("condition 2, " );
-    correction=angle;
-    Serial.print("correction");
-    analogWrite(pwm_2, 140+angle); 
-    analogWrite(pwm_1, 140-angle);
+  if(error>0){
+    Serial.println("condition 2: " );
+    analogWrite(pwm_2, 140+abs(error)); 
+    analogWrite(pwm_1, 140-abs(error));
   }
-   if(angle<0){
-    Serial.println("condition 3");
-    correction=angle;
-    Serial.print("correction");
-    analogWrite(pwm_2, 140-abs(angle)); 
-    analogWrite(pwm_1, 140+abs(angle));
+   if(error<0){
+    Serial.println("condition 3: ");
+    analogWrite(pwm_2, 140-abs(error)); 
+    analogWrite(pwm_1, 140+abs(error));
   }
-
-
-
-
+  Serial.println(error);
   delay(10);
 }
 
