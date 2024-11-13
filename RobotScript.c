6ffe2d49b8ad4839143835ffa5b917b2198b133e
AdamGr <134016956+PapaAzor@users.noreply.github.com>
2024-11-13 22:14:55 +0100

    Update RobotScript.c

--- RobotScript.c
+++ RobotScript.c
@@ -4,24 +4,27 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-// Not all of those libraries are necessary , but I dont remember which
+
 #define PORT 8000  // Define the port for TCP connection
+#define BUFFER_SIZE 1024
 
 int sock = 0;
 struct sockaddr_in serv_addr;
+char buffer[BUFFER_SIZE] = {0};
 
-
-
-
-// Function to send a command to the server
 void send_command(const char *command) {
     send(sock, command, strlen(command), 0);  // Send command to server
 }
 
-/*void *camera_thread_function(void *arg){
-    system("rpicam-vid -t 0 -n --framerate 20 --saturation 0 --denoise cdn_fast --contrast 0.2 --sharpness 0.1 --inline -o - | gst-launch-1.0 fdsrc fd=0 ! udpsink host=192.168.28.196 port=6433");
+// Thread to receive data from the server
+void *receive_data_thread_function(void *arg) {
+    int valread;
+    while ((valread = read(sock, buffer, BUFFER_SIZE - 1)) > 0) {
+        buffer[valread] = '\0';  // Null-terminate the received string
+        printf("Received data from server: %s\n", buffer);  // Print the received data
+    }
     return NULL;
-}*/
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -32,7 +35,7 @@ int main(int argc, char *argv[]) {
 
     // Create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\nSocket creation error\n");
+        perror("Socket creation error");
         return -1;
     }
 
@@ -41,36 +44,24 @@ int main(int argc, char *argv[]) {
 
     // Convert IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported\n");
+        perror("Invalid address/ Address not supported");
         return -1;
     }
 
-    // Connect to the server
+
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed\n");
+        perror("Connection failed");
         return -1;
     }
 
-    
-char str[]="uddp";// get this from algorithm
-send_command(str);
-        
-        
-    
 
-    // Uncomment the camera thread code if needed:
-    /*
-    pthread_t camera_thread;
-    if (pthread_create(&camera_thread, NULL, camera_thread_function, NULL) != 0) {
-        fprintf(stderr, "Error creating camera thread\n");
-        return 1;
-    } 
-    
-    if (pthread_join(camera_thread, NULL) != 0) {
-        fprintf(stderr, "Error joining camera thread\n");
+    pthread_t receive_thread;
+    if (pthread_create(&receive_thread, NULL, receive_data_thread_function, NULL) != 0) {
+        fprintf(stderr, "Error creating receive thread\n");
         return 1;
     }
-    */
+
+    pthread_join(receive_thread, NULL);
 
     // Close the socket
     close(sock);
