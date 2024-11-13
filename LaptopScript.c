ae14a39bce4e361f61c4e845b4ae76b936f072e5
AdamGr <134016956+PapaAzor@users.noreply.github.com>
2024-11-13 15:05:37 +0100

    Update LaptopScript.c

--- LaptopScript.c
+++ LaptopScript.c
@@ -13,12 +13,6 @@ struct sockaddr_in address;
 int addrlen = sizeof(address);
 char buffer[BUFFER_SIZE] = {0};
 
-/*void *receive_image_thread_function(void *arg) {
-    // Use GStreamer to receive and display the video stream
-    system("gst-launch-1.0 udpsrc address=0.0.0.0 port=6433 ! h264parse ! decodebin ! autovideosink");
-    return NULL;
-}*/
-
 void setup() {
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
@@ -40,9 +34,19 @@ void setup() {
         close(server_fd);
         exit(EXIT_FAILURE);
     }
-
 }
-int startingPos[2];
+
+void send_command(int socket, const int *command, size_t num_elements) {
+    size_t length = num_elements * sizeof(int);
+    ssize_t bytes_sent = send(socket, command, length, 0);
+    if (bytes_sent == -1) {
+        perror("send failed");
+    } else {
+        printf("Sent %ld bytes\n", bytes_sent);
+    }
+}
+
+int startingPos[3] = {0, 0, 0};
 char endPos[2];
 
 void *tcp_server_thread_function(void *arg) {
@@ -56,79 +60,73 @@ void *tcp_server_thread_function(void *arg) {
         #define arr_size 126
         int arr_index = 0;
         char arr[arr_size] = {0};
-        
-        
+
         while ((valread = read(new_socket, buffer, BUFFER_SIZE - 1)) > 0) {
-            buffer[valread] = '\0';  // Null-terminate the received data
-            printf("Received endpoint data: %s\n", buffer);
+            buffer[valread] = '\0';
+            printf("Received data: %s\n", buffer);
 
-             
             for (int i = 0; i < valread; i++) {
-                if (arr_index < arr_size - 1) {  
-                    arr[arr_index] = buffer[i];  
-                    printf("Added to arr[%d]: %c\n", arr_index, arr[arr_index]);  
-                    arr_index++;  
+                if (arr_index < arr_size - 1) {
+                    arr[arr_index] = buffer[i];
+                   // printf("\nAdded to arr[%d]: %c", arr_index, arr[arr_index]);
+                    arr_index++;
                 }
-            }
-            if (arr[0] == 'u') {
-                printf("\nGoing Up");
-            } else if (arr[0] == 'd') {
-                printf("\nGoing Down");
-            } else if (arr[0] == 'l') {
-                printf("\nGoing Left");
-            } else if (arr[0] == 'r') {
-                printf("\nGoing Right");
+				if(arr[0]!='s' && arr[0]!='e'){
+					if (arr[i] == 'u') {
+						printf("\nGoing Up");
+					} else if (arr[i] == 'd') {
+						printf("\nGoing Down");
+					} else if (arr[i] == 'l') {
+						printf("\nGoing Left");
+					} else if (arr[i] == 'r') {
+						printf("\nGoing Right");
+					} else {
+                    printf("\nUnknown command");
+					}
+			}
             }
 
-            // e for endpoint
             if (arr[0] == 'e') {
-                endPos[0] = arr[1];  
-                endPos[1] = arr[3];  
-               // printf("endPos[0]: %c, endPos[1]: %c\n", endPos[0], endPos[1]);  
+                endPos[0] = arr[1];
+                endPos[1] = arr[3];
             }
             if (arr[0] == 's') {
-                startingPos[0] = arr[2]-'0';  
-                startingPos[1] = arr[5]-'0';  
-                //printf("startingPos[0]: %d, startingPos[1]: %d\n", startingPos[0], startingPos[1]); 
+                startingPos[0] = arr[2] - '0';
+                startingPos[1] = arr[5] - '0';
+                startingPos[2] = arr[8] - '0';
+                startingPos[2] *= 90;
+                printf("startingPos[0]: %d, startingPos[1]: %d, startingPos[2]: %d\n", startingPos[0], startingPos[1], startingPos[2]);
+                size_t num_elements = sizeof(startingPos) / sizeof(startingPos[0]);
+				send_command(new_socket, startingPos, num_elements);
             }
-            printf("\nendPos[0]: %c, endPos[1]: %c\n", endPos[0], endPos[1]);
-			printf("\nstartingPos[0]: %d, startingPos[1]: %d\n", startingPos[0], startingPos[1]); 
+            
             
         }
 
         if (valread == 0) {
             printf("\nClient disconnected.\n");
         }
-        close(new_socket); 
+
+        close(new_socket);
     }
-    
+
     return NULL;
 }
 
 int main() {
     setup();
-    
-    pthread_t tcp_server_thread;//,receive_image_thread;
-    
-    if (pthread_create(&tcp_server_thread, NULL, tcp_server_thread_function, NULL) != 0) {
-        
-        return 1;
-    } 
-    /*if (pthread_create(&receive_image_thread, NULL, receive_image_thread_function, NULL) != 0) {
-        fprintf(stderr, "Error creating image thread\n");
-        return 1;
-    }*/
 
-    
+    pthread_t tcp_server_thread;
+
+    if (pthread_create(&tcp_server_thread, NULL, tcp_server_thread_function, NULL) != 0) {
+        fprintf(stderr, "Error creating TCP server thread\n");
+        return 1;
+    }
+
     if (pthread_join(tcp_server_thread, NULL) != 0) {
         fprintf(stderr, "Error joining TCP server thread\n");
         return 1;
     }
-    
-    /* if (pthread_join(receive_image_thread, NULL) != 0) {
-        fprintf(stderr, "Error joining image thread\n");
-        return 1;
-    }*/
 
     close(server_fd);
     return 0;
