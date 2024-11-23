c85c3714bf2a029cddb97a9b6014c40850886274
BernardMoc <119946085+BernardMoc@users.noreply.github.com>
2024-11-23 12:43:14 +0100

    UpdatedCalgorythm.c

--- tryingchangealgorythm.c
+++ tryingchangealgorythm.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+int main(int argc, char *argv[]) {
 int mapway[2]={0,0};
-char startPos[2]={'3','4'};
-int startPosReceived[2]={0,0};
-char endPos[2]={'1','1'};
-int endPosReceived[2]={0,0};
+char *startPos = argv[1];
+    int startPosReceived[3];
+    char *endPos = argv[2];
+    int endPosReceived[2];
 char instructions[127]={'0'};
 char drive[127]={'0'};
 int a=0;
-    int main() {
-    for (int i = 0; i<2; i++) {
+    /*for (int i = 0; i<2; i++) {
         startPosReceived[i] = startPos[i]- '0';  // Convert char to int by subtracting '0'
          printf("startPosReceived[%d] = %d\n", i, startPosReceived[i]);
     }
@@ -21,7 +21,7 @@ int a=0;
     }
 for (int i = 0; i<2; i++) {
      printf("directions[%d] = %d\n",i,mapway[i]=endPosReceived[i]-startPosReceived[i]);
-  }
+  }*/
   mapway[1]=endPosReceived[1]-startPosReceived[1];
   mapway[0]=endPosReceived[0]-startPosReceived[0];
   
@@ -31,13 +31,11 @@ while(mapway[0]!=0)
      {mapway[0]=mapway[0]-1;
         instructions[a]='r';
          a++;
-        
      }
      else
      {mapway[0]=mapway[0]+1;
           instructions[a]='l';
-         a++;
-          
+         a++;   
      }
     }
     while(mapway[1]!=0)
@@ -45,7 +43,6 @@ while(mapway[0]!=0)
      if (mapway[1]>0)
      {mapway[1]=mapway[1]-1;
           instructions[a]='u';
-     
          a++;
      }
      else
@@ -55,12 +52,12 @@ while(mapway[0]!=0)
      }
     }
 instructions[a]='e';
-for (int i = 0; i < 128; i++) 
+/*for (int i = 0; i < 128; i++) 
   {
       if(instructions[i]=='e')
       {break;}
     printf("%c\n", instructions[i]);
-  }
+  }*/
   
 
 char direction = 'u'; // Initial direction
@@ -96,10 +93,11 @@ char direction = 'u'; // Initial direction
             }
         }
     }
-    for (int i = 0; i < 128; i++) 
+    /*for (int i = 0; i < 128; i++) 
   {
       if(drive[i]=='E')
       {break;}
     printf("%c\n", drive[i]);
-  }
+  }*/
+return 0;
 }
